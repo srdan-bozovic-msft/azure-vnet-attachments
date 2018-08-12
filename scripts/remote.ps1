f432b1ef44f965cc918d80ed3da3308c6245d3e5
Srđan Božović <srdjan.bozovic@live.com>
2018-08-12 20:06:05 +0200

    add for test

---
+++ scripts/remote.ps1
@@ -0,0 +1,26 @@
+﻿param([Parameter(Mandatory=$true)][String]$subscriptionId, [Parameter(Mandatory=$true)][String]$resourceGroupName, [Parameter(Mandatory=$true)][String]$virtualNetworkName)
+
+function Ensure-Login () 
+{
+    $context = Get-AzureRmContext
+    If($context.Subscription -eq $null)
+    {
+        Login-AzureRmAccount | Out-null
+    }
+}
+
+Ensure-Login
+
+$context = Get-AzureRmContext
+If($context.Subscription.Id -ne $subscriptionId)
+{
+    #TODO check if subscription exists
+    Select-AzureRmSubscription -SubscriptionId $subscriptionId  | Out-null
+}
+
+#TODO check if vnet exists
+$virtualNetwork = Get-AzureRmVirtualNetwork -ResourceGroupName $resourceGroupName -Name $virtualNetworkName
+
+$virtualNetwork.AddressSpace.AddressPrefixes[-1]
+
+
